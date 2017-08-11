eef39a160438692c8bbb09a16dae138c1846016e
jlguevara99 <jlguevara99@hotmail.com>
2017-08-11 15:04:14 -0600

    funcion de tamaño

--- main.cpp
+++ main.cpp
@@ -5,9 +5,37 @@
 #include<iostream>
 #include <string>
 using namespace std;
-int main(){
-	Bicicleta* bi = new Bicicleta();
+int tamano(int);
 
+void llenado1();
+void llenado2();
+
+int main(){
+	BicicletaCarrera** camionCar = NULL;
+	BicicletaMontana** camionMon = NULL;
+	cout<<"Buenas, Bienvenido a Jitensha Motors"<<endl;
+	cout<<"Ingrese el tamaño n del camion de carreras"<<endl;
+	int tamanoCar;
+	cin>>tamanoCar;
+	cout<<"Ingrese el tamaño n del camion de montaña"<<endl;
+	int tamanoMon;
+	cin>>tamanoMon;
+
+	
 
 	return 0;
+}
+
+int tamano(int numero){
+	int m;
+	if(numero%4 == 0){
+		m = numero + (numero*0.75);
+	}else if(numero%2 == 0){
+		m = numero*1.5;
+	}else{
+		m = 2*numero;
+	}
+
+	return m;
+	
 }
